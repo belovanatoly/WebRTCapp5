d9d6584720acfa56dccaa38acade113fa45be630
belovanatoly <61622458+belovanatoly@users.noreply.github.com>
2020-03-10 20:17:43 +0300

    Update usermob.js

--- usermob.js
+++ usermob.js
@@ -5,26 +5,21 @@ var streamConstraints = { "audio": false, "video": true };
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-//function getUserMedia_click() {
-//document.getElementById('debug').innerHTML += 'getUserMedia_click<br>';
-//  console.log("getUserMedia_click()");
   navigator.getUserMedia(
     streamConstraints,
     getUserMedia_success,
     getUserMedia_error
   );
-//}
 
 function getUserMedia_success(stream) {
-  //console.log("getUserMedia_success():", stream);
-  document.getElementById('debug').innerHTML += 'getUserMedia_success';
-  localVideo.src = URL.createObjectURL(stream); // Подключаем медиапоток к HTML-элементу <video>
+
+  document.getElementById('debug').innerHTML += 'getUserMedia_success 2';
+  localVideo.src = URL.createObjectURL(stream); 
 //  localVideo1.srcObject = stream;
-  localStream = stream; // и сохраняем в глобальной переменной для дальнейшего использования
 }
 
 function getUserMedia_error(error) {
- // console.log("getUserMedia_error():", error);
-//document.getElementById('debug').innerHTML += error;
+ console.log("getUserMedia_error():", error);
+// document.getElementById('debug').innerHTML += error;
 }
 
